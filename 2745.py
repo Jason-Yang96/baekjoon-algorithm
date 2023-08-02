7660877024ff8b7711d564e257e7b2cb7c24320f
Jason <didwotjs120@gmail.com>
2023-08-02 12:49:34 +0900

    Update 2745.py
    직접 진법 전환 코드 추가

--- 2745.py
+++ 2745.py
@@ -1,21 +1,25 @@
 """ 
 ZZZZZ = 35*36**4 + 35*36**3 + 35*36**2 + 35*36**1 + 35
 """
-""" def base_n(a, b):
+def base_n(a, b):
+    input_list = list(a)
     sum_list = []
-    for i in range(len(a)):
-        digit = ord(a[i]) - 55
-        sum_list.append(digit*int(b)**(len(a)-i+1))
+    for i in range(len(input_list)):
+        if input_list[i].isalpha():     
+            digit = ord(input_list[i]) - 55
+            sum_list.append(digit*int(b)**(len(input_list)-i-1))
+        else:
+            sum_list.append(int(input_list[i])*int(b)**(len(input_list)-i-1))
     return sum(sum_list), sum_list
 n, base = input().split()
 
-print(base_n(n, int(base))) """
+print(base_n(n, int(base))) 
 
-def base_n(a, b):
+""" def base_n(a, b):
     return int(a,b)
 
 n, base = input().split()
 print(base_n(n, int(base)))
-
+ """
 
 
